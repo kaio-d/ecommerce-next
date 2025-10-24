a74e6b7bf8af66880aec043e77f414d2e753f880
kaio-d <dantaskaio01@gmail.com>
2025-10-24 15:36:11 -0300

    Custom or header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import { Search, ShoppingBag } from "lucide-react";
+import Image from "next/image";
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          DevStore
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-slate-900 px-5 py-3 ring-zinc-700">
+          <Search className="w-5 h-5 text-zinc-500" />
+          <input
+            type="text"
+            placeholder="Buscar produtos"
+            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+          />
+        </form>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="w-4 h-4" />
+          <span className="text-sm">Cart (0)</span>
+        </div>
+
+        <div className="w-px h-4 bg-zinc-700"></div>
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Accont</span>
+          <Image
+            src="https://github.com/kaio-d.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt=""
+          />
+        </Link>
+      </div>
+    </div>
   );
 }
