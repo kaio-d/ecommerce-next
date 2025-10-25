e2899bc1260dba3cb6fe8dace47e05f4d85d876d
kaio-d <dantaskaio01@gmail.com>
2025-10-25 16:06:50 -0300

    Create producs details

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ["var(--font-inter)", "sans-serif"],
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
     },
   },
   plugins: [],
