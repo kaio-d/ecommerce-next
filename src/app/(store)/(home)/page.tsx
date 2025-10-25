75055805632c3f3d28f9b185a6cdc5862d1af61e
kaio-d <dantaskaio01@gmail.com>
2025-10-25 11:45:44 -0300

    create fetch api

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,15 +1,28 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api("/products/featured");
+
+  const products = await response.json();
+  
+  return products;
+}
+
+export default async function Home() {
+  const [hightlightedProduct, ...otherproducts] = await getFeaturedProducts();
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/products/${hightlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
-          src="/moletom-never-stop-learning.png"
+          src={hightlightedProduct.image}
           className="h-full w-auto group-hover:scale-105 transition-transform duration-500"
           width={860}
           height={860}
@@ -18,19 +31,25 @@ export default function Home() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moleton Never Stop Learning</span>
+          <span className="text-sm truncate">{hightlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {hightlightedProduct.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
           </span>
         </div>
       </Link>
 
-      <Link
-        href="/"
+      {otherproducts.map(product => {
+        return (
+          <Link
+          key={product.id}
+        href={`/products/${product.slug}`}
         className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
-          src="/moletom-java.png"
+          src={product.image}
           className="h-full w-auto group-hover:scale-105 transition-transform duration-500"
           width={860}
           height={860}
@@ -39,33 +58,16 @@ export default function Home() {
         />
 
         <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moleton Never Stop Learning</span>
+          <span className="text-sm truncate">{product.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
           </span>
         </div>
       </Link>
-
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-      >
-        <Image
-          src="/camiseta-dowhile-2022.png"
-          className="h-full w-auto group-hover:scale-105 transition-transform duration-500"
-          width={860}
-          height={860}
-          quality={100}
-          alt=""
-        />
-
-        <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moleton Never Stop Learning</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div>
-      </Link>
+      )})}
     </div>
   );
 }
